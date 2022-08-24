4aa7f6b738e4cfa8dd0faed9087ab76c0c7fd61b
lautaro-osacar <lautaro.eo@gmail.com>
2022-08-24 10:59:38 -0300

    feat(redux): redux integration ART-367

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 import { WalletConnectClientContextProvider } from "../contexts/WalletConnectClientContext";
+import { store } from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WalletConnectClientContextProvider>
-      <Component {...pageProps} />
-    </WalletConnectClientContextProvider>
+    <Provider store={store}>
+      <WalletConnectClientContextProvider>
+        <Component {...pageProps} />
+      </WalletConnectClientContextProvider>
+    </Provider>
   );
 }
 
