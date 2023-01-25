17c03117aa75d22db6d1f27c8dcfccbf51fcd089
Jenyfair <ljenyfair@gmail.com>
2023-01-25 10:29:19 +0100

    feat(soulboundtoken): add back btn THUB-466

--- components/templates/CreateNftTemplate/index.tsx
+++ components/templates/CreateNftTemplate/index.tsx
@@ -10,6 +10,8 @@ import Textarea from "../../atoms/Textarea";
 import { ICreateNftTemplate, INftFormResult } from "./types";
 import FileForm from "../../molecules/FileForm";
 import Image from "next/image";
+import IconButton from "../../atoms/IconButton";
+import { useRouter } from "next/router";
 
 const CreateNftTemplate: React.FC<ICreateNftTemplate> = ({
   onSubmit,
@@ -19,7 +21,7 @@ const CreateNftTemplate: React.FC<ICreateNftTemplate> = ({
   isSoulBound,
 }) => {
   const [isPreviewVisible, setIsPreviewVisible] = useState<boolean>();
-
+  const router = useRouter();
   const schema = yup
     .object({
       name: yup.string().required().label("Name"),
@@ -115,7 +117,7 @@ const CreateNftTemplate: React.FC<ICreateNftTemplate> = ({
 
   const onClickSubmit = () => {
     handleSubmit((formResponse) => {
-      onSubmit({ result: formResponse, formData, isSoulBound:isSoulBound??false });
+      onSubmit({ result: formResponse, formData, isSoulBound: isSoulBound ?? false });
     })();
   };
 
@@ -123,7 +125,7 @@ const CreateNftTemplate: React.FC<ICreateNftTemplate> = ({
     <React.Fragment>
       <div className="flex flex-col justify-center h-[max-content]">
         {isSoulBound && (
-          <div className="bg-gray-500 p-s20 flex-col rounded-[20px] my-s24 max-w-[896px]">
+          <div className="bg-gray-500 p-s20 flex-col rounded-[20px] mt-s24 mb-s40 max-w-[896px]">
             <div className="bg-gray-700 rounded-[8px] px-s16 py-s8 inline-flex">
               <Text
                 text="Good to know !"
@@ -170,7 +172,9 @@ const CreateNftTemplate: React.FC<ICreateNftTemplate> = ({
         )}
 
         <div className="flex flex-col md:flex-row-reverse justify-center h-[max-content]">
+
           <div className="md:ml-s24 md:mb-[0px] flex flex-col justify-start">
+
             <Controller
               control={control}
               name="file"
@@ -206,91 +210,104 @@ const CreateNftTemplate: React.FC<ICreateNftTemplate> = ({
               />
             )}
           </div>
-          <div
-            className={`bg-gray-500 px-s16 md:px-s32 py-s28 md:py-s32 rounded-[20px] w-full md:inline-flex md:flex-col md:w-auto ${
-              noTitle ? "!pt-[0px]" : ""
-            }`}
-          >
-            {!noTitle && !isSoulBound ? (
-              <Text text="Create your NFT" type="h3" weight="bold" />
-            ) : (
-              !noTitle &&
-              isSoulBound && (
-                <Text
-                  text="Create your soulbound Token"
-                  type="h3"
-                  weight="bold"
-                />
-              )
-            )}
-            <form className="mt-s4 md:mt-s8 flex flex-col flex-1">
-              <Input
-                id="name"
-                label="Name"
-                placeholder="NFT's name"
-                required={true}
-                error={errors.name?.message}
-                {...register("name")}
+          <div className="relative">
+            <div
+              className={`bg-gray-500 px-s16 md:px-s32 py-s28 md:py-s32 rounded-[20px] w-full md:inline-flex md:flex-col md:w-auto ${noTitle ? "!pt-[0px]" : ""
+                }`}
+            >
+
+              <IconButton
+                onClick={() => router.back()}
+                text="Back"
+                icon="ArrowLeft"
+                iconSize={16}
+                iconColor="white"
+                size="xsmall"
+                autoWidth
+                type="primary"
+                className="rounded-[20px] absolute -left-s8 -top-s16 maxmd:hidden"
               />
-              <Textarea
-                id="description"
-                label="Description"
-                required={true}
-                placeholder="Tell about the NFT in a few words"
-                rows={5}
-                style={{ resize: "none" }}
-                error={errors.description?.message}
-                {...register("description")}
-              />
-              <Input
-                id="quantity"
-                label="Quantity"
-                required={true}
-                placeholder="Maximum 1000"
-                error={errors.quantity?.message}
-                inputType="number"
-                min={1}
-                max={1000}
-                type={noQuantity ? "disabled" : "primary"}
-                {...register("quantity")}
-              />
-              {!isSoulBound && (
+              {!noTitle && !isSoulBound ? (
+                <Text text="Create your NFT" type="h3" weight="bold" />
+              ) : (
+                !noTitle &&
+                isSoulBound && (
+                  <Text
+                    text="Create your soulbound Token"
+                    type="h3"
+                    weight="bold"
+                  />
+                )
+              )}
+              <form className="mt-s4 md:mt-s8 flex flex-col flex-1">
                 <Input
-                  id="royalty"
-                  label="Royalty"
-                  placeholder="Ex: 8%"
+                  id="name"
+                  label="Name"
+                  placeholder="NFT's name"
                   required={true}
-                  error={errors.royalty?.message}
+                  error={errors.name?.message}
+                  {...register("name")}
+                />
+                <Textarea
+                  id="description"
+                  label="Description"
+                  required={true}
+                  placeholder="Tell about the NFT in a few words"
+                  rows={5}
+                  style={{ resize: "none" }}
+                  error={errors.description?.message}
+                  {...register("description")}
+                />
+                <Input
+                  id="quantity"
+                  label="Quantity"
+                  required={true}
+                  placeholder="Maximum 1000"
+                  error={errors.quantity?.message}
+                  inputType="number"
+                  min={1}
+                  max={1000}
+                  type={noQuantity ? "disabled" : "primary"}
+                  {...register("quantity")}
+                />
+                {!isSoulBound && (
+                  <Input
+                    id="royalty"
+                    label="Royalty"
+                    placeholder="Ex: 8%"
+                    required={true}
+                    error={errors.royalty?.message}
+                    defaultValue={undefined}
+                    inputType="number"
+                    min={0}
+                    max={100}
+                    {...register("royalty")}
+                  />
+                )}
+                <Input
+                  id="collectionId"
+                  label="Collection ID"
+                  placeholder="Id of the collection"
+                  error={errors.collectionId?.message}
                   defaultValue={undefined}
                   inputType="number"
-                  min={0}
-                  max={100}
-                  {...register("royalty")}
+                  {...register("collectionId")}
                 />
-              )}
-              <Input
-                id="collectionId"
-                label="Collection ID"
-                placeholder="Id of the collection"
-                error={errors.collectionId?.message}
-                defaultValue={undefined}
-                inputType="number"
-                {...register("collectionId")}
-              />
-              
 
-              <div className="flex flex-1 items-end">
-                <Button
-                  color="gray-50"
-                  text={isSoulBound ? "Create SBT" : "Create NFT"}
-                  type="primary"
-                  size="medium"
-                  className="mt-s20 md:mt-s32 bg-gray-300"
-                  onClick={onClickSubmit}
-                  disabled={disabled}
-                />
-              </div>
-            </form>
+
+                <div className="flex flex-1 items-end">
+                  <Button
+                    color="gray-50"
+                    text={isSoulBound ? "Create SBT" : "Create NFT"}
+                    type="primary"
+                    size="medium"
+                    className="mt-s20 md:mt-s32 bg-gray-300"
+                    onClick={onClickSubmit}
+                    disabled={disabled}
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
